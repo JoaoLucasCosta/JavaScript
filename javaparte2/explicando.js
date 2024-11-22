6d0c745abef5569b7264d0b530326e6f6ca2cc84
João Lucas Costa <lc3680567@gmail.com>
2024-11-22 20:43:41 -0300

    Update and rename explicando.html to explicando.js

--- javaparte2/explicando.js
+++ javaparte2/explicando.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-        
-<script>
-    
     //if: Executa um bloco de código se a condição for verdadeira.
     //else: Executa outro bloco de código se a condição for falsa.
     let numero = prompt("Digite um número:");
@@ -48,11 +37,3 @@
     } else {
         alert("Você errou! O número era " + numeroSecreto);
     }
-
-
-</script>
-
-
-    </script>
-</body>
-</html>
